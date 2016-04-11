ab99978e5988e21041df26bf6ab1faa97c5aca3a
Gasfar Mukhametdinov <gasfarmg@hotmail.com>
2016-04-11 18:53:01 +0300

    Add first project

---
+++ TelegramBots/TelegramBots/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBots
+{
+	class Program
+	{
+		public string botToken { get; set; }
+
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
